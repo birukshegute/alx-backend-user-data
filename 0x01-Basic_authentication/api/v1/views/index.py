ecadd057e0ffe1a78f0efd729e02d7e7b17006af
birukshegute <birukshegute@gmail.com>
2024-08-07 06:01:36 +0000

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,10 @@ def unauthorized() -> str:
     """ Raises a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Raises a 403 error by using abort
+    """
+    abort(403)
