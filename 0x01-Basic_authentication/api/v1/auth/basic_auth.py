cbf719c315993a84c668daf091a224982a9b354a
birukshegute <birukshegute@gmail.com>
2024-08-07 07:55:19 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,15 @@ from .auth import Auth
 
 
 class BasicAuth(Auth):
-    """Empty for now"""
+    """A class that inherits from Auth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header"""
+        if not authorization_header:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
