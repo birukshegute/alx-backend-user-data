94638d37408a9f11bab2f1d3e3493b33fb3d1a73
birukshegute <birukshegute@gmail.com>
2024-08-07 06:28:23 +0000

    task 3 trial 1

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Manages the API authentication.
+"""
+
+from flask import request
+
+
+class Auth
+    """ Class to manage manage the API authentication. """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False - path and excluded_paths that will be used later"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """that returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """that returns None"""
+        return None
