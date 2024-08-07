0b7f2a357cedc0991dd241f8acaac504fc6be4ce
birukshegute <birukshegute@gmail.com>
2024-08-07 08:22:54 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,13 +4,14 @@ A class BasicAuth that inherits from Auth
 """
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """A class that inherits from Auth"""
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header"""
         if not authorization_header:
             return None
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
         if authorization_header[:6] != "Basic ":
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of base64_authorization_header:"""
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
