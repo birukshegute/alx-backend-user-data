8b75e9bf48d228199e1a8c3c7838b0b91aabb86c
birukshegute <birukshegute@gmail.com>
2024-08-09 12:52:23 +0000

    task 0 trial 5

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,8 +27,13 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        user = request.current_user
+        return jsonify(user.to_json())
     user = User.get(user_id)
-    if user is None:
+    if user is None or request.current_user is None:
         abort(404)
     return jsonify(user.to_json())
 
@@ -120,12 +125,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
-
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def get_me() -> str:
-    """ GET /api/v1/users/me
-    """
-    if request.current_user is None:
-        abort(404)
-    return jsonify(request.current_user.to_json())
